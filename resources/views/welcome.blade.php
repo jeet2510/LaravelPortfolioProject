be918560650003eecdbcd989ef0f644db40da367
“jeet2510” <pandyajeetu2510@gmail.com>
2023-09-27 18:48:25 +0530

    Few changes in Welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,22 +10,22 @@
                 <div class="card-header text-white bg-primary">{{ __('Welcome to my practice application for Laravel') }}</div>
 
                 <div class="card-body">
-                    <h2>{{ __('Laravel Practice tasks') }}</h2>
+                    <h2>{{ __('Laravel Practice Projects') }}</h2>
                     <ul class="list-group">
-                        <li class="list-group-item">Task 1: Dashboard for Admin
+                        <li class="list-group-item">Projects 1: Dashboard for Admin
                         <br>
                         <p>Dashboard and Chart of Users. </p>
                         </li>
-                        <li class="list-group-item">Task 2: User Management Application
+                        <li class="list-group-item">Projects 2: User Management Application
                         <br>
                         <p>Admin can manage, search, sort, change status, bulk-actions over users data. </p>
                         </li>
-                        <li class="list-group-item">Task 3: Data To Excel And PDF Files
+                        <li class="list-group-item">Projects 3: Data To Excel And PDF Files
                         <br>
                         <p>Admin can download data of users in PDF and Excel formats. </p>
                         </li>
                         
-                        <li class="list-group-item">Task 4: Ticker Genration application
+                        <li class="list-group-item">Projects 4: Ticker Genration application
                         <br>
                         <p>User can genrate tickets, While Adin can review and comment or close the tickets. </p>
                         </li>    
