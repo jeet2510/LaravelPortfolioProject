c31178780b6c1cab25629a3565fea71802c1e8b6
“jeet2510” <pandyajeetu2510@gmail.com>
2023-10-05 13:38:02 +0530

    Move redirect link to ENV

--- config/services.php
+++ config/services.php
@@ -39,7 +39,7 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRETE_KEY'),
-        'redirect' => 'http://127.0.0.1:8000/callback/google',
+        'redirect' => env('GOOGLE_LOGIN_REDIRECT'),
     ], 
 
 ];
